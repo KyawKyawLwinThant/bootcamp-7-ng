11bcd3e87bb73e8a88e7502c1126ae99ddcfa6dd
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-01-31 09:04:34 +0630

    input output 2

---
+++ src/app/notifications-button/notifications-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './notifications-button.component.html',
+  styleUrl: './notifications-button.component.scss'
+})
+export class NotificationsButtonComponent {
+    @Input()
+    count=0;
+}
---
+++ src/app/notifications-manager/notifications-manager.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-manager',
+  standalone: true,
+  imports: [
+  
+  ],
+  templateUrl: './notifications-manager.component.html',
+  styleUrl: './notifications-manager.component.scss'
+})
+export class NotificationsManagerComponent {
+  @Input()
+  count = 0;
+  @Output()
+  countChange =new EventEmitter<number>();
+
+  public addNotification(){
+    this.count++;
+    this.countChange.emit(this.count);
+  }
+  public removeNotification(){
+    if(this.count == 0){
+      return;
+    }
+    this.count--;
+    this.countChange.emit(this.count);
+  }
+  public resetCount(){
+    this.count = 0;
+    this.countChange.emit(this.count);
+  }
+}

2fe3f65562ea50e912f6cef8fd56e2e8ec791f40
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-01-31 08:57:38 +0630

    data bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ChildComponent],
 })
 export class AppComponent {
-  title = 'data-bind';
+  greeting = 'Hello!Angular....';
+  imgUrl =
+    'https://images.unsplash.com/photo-1706354924659-582cec69ce3b?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+  switch: boolean = true;
+
+  public onOff() {
+    this.switch = !this.switch;
+  }
+
+  public getFromChild(msg: string) {
+    this.greeting = msg;
+  }
+
+  student = {
+    name: 'John Doe',
+    hobby: 'Reading',
+  };
+  public sayHello() {
+    return 'Life is beautiful!';
+  }
 }
